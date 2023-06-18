172827a17beb822d9cbbce468566318f954be91c
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-06-18 19:30:53 +0200

    add matches for 1st round 2023/24

---
+++ src/app/2023-24/page.tsx
@@ -0,0 +1,66 @@
+import { jsx } from 'theme-ui'
+
+export default function DFBPokal_2023_24() {
+  return (
+    <main>
+      <h1>DFB-Pokal 2023/24</h1>
+      <h2>1. Hauptrunde</h2>
+      <h3>Oberliga – 2. Bundesliga (3)</h3>
+      <ul>
+        <li>SV Atlas Delmenhorst – FC St. Pauli</li>
+        <li>FC Rot-Weiß Koblenz – 1. FC Kaiserslautern</li>
+        <li>FC Oberneuland – 1. FC Nürnberg</li>
+      </ul>
+      <h3>Oberliga – 1. Bundesliga (4)</h3>
+      <ul>
+        <li>TuS Makkabi Berlin – VfL Wolfsburg</li>
+        <li>Rostocker FC – 1. FC Heidenheim</li>
+        <li>SV Oberachern – SC Freiburg</li>
+        <li>TuS Bersenbrück – Borussia Mönchengladbach</li>
+      </ul>
+      <h3>Regionalliga – 2. Bundesliga (5)</h3>
+      <ul>
+        <li>FV Illertissen – Fortuna Düsseldorf</li>
+        <li>FC Energie Cottbus – SC Paderborn 07</li>
+        <li>FSV Frankfurt – FC Hansa Rostock</li>
+        <li>FC Carl Zeiss Jena – Hertha BSC Berlin</li>
+        <li>FC Gütersloh – Holstein Kiel</li>
+      </ul>
+      <h3>Regionalliga – 1. Bundesliga (6)</h3>
+      <ul>
+        <li>FC 08 Homburg – SV Darmstadt 08</li>
+        <li>TSG Balingen – VfB Stuttgart</li>
+        <li>TSV Schott Mainz – Borussia Dortmund</li>
+        <li>1. FC Lokomotive Leipzig – Eintracht Frankfurt</li>
+        <li>FC Astoria Walldorf – 1. FC Union Berlin</li>
+        <li>FC Teutonia 05 Ottensen – Bayer 04 Leverkusen</li>
+      </ul>
+      <h3>3. Liga – 2. Bundesliga (5)</h3>
+      <ul>
+        <li>Hallescher FC – SpVgg Greuther Fürth</li>
+        <li>SV Sandhausen – Hannover 96</li>
+        <li>SSV Jahn Regensburg – 1. FC Magdeburg</li>
+        <li>Rot-Weiss Essen – Hamburger SV</li>
+        <li>1. FC Saarbrücken – Karlsruher SC</li>
+      </ul>
+      <h3>3. Liga – 1. Bundesliga (5)</h3>
+      <ul>
+        <li>SpVgg Unterhaching – FC Augsburg</li>
+        <li>VfB Lübeck – TSG 1899 Hoffenheim</li>
+        <li>FC Viktoria Köln – SV Werder Bremen</li>
+        <li>Arminia Bielefeld – VfL Bochum</li>
+        <li>Preußen Münster – FC Bayern München</li>
+      </ul>
+      <h3>2. Bundesliga – 2. Bundesliga (1)</h3>
+      <ul>
+        <li>Eintracht Braunschweig – FC Schalke 04</li>
+      </ul>
+      <h3>2. Bundesliga – 1. Bundesliga (3)</h3>
+      <ul>
+        <li>VfL Osnabrück – 1. FC Köln</li>
+        <li>SV Elversberg – 1. FSV Mainz 05</li>
+        <li>SV Wehen Wiesbaden – RB Leipzig</li>
+      </ul>
+    </main>
+  )
+}
