179f616e9b1e8d8b3705cf143e65399d5a6baec7
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-06-20 23:00:03 +0200

    better sorting, first eight matches of second round 2022/23

--- src/app/2022-23/page.tsx
+++ src/app/2022-23/page.tsx
@@ -7,8 +7,7 @@ type Match = {
   homeLeague: string | number
   awayLeague: string | number
   result?: string
-  homeWinner?: boolean
-  awayWinner?: boolean
+  identifier?: number
 }
 
 type Data = { [key: string]: Match[] }
@@ -33,59 +32,104 @@ export default function DFBPokal_2023_24() {
     '5RS': 'Oberliga Rheinland-Pfalz/Saar',
   }
   const data: Data = {
+    '2. Hauptrunde': [
+      { home: 'VfB Lübeck', homeLeague: '4N', away: '1. FSV Mainz 05', awayLeague: 1, result: '0:3', identifier: 2 },
+      { home: 'Stuttgarter Kickers', homeLeague: '5BW', away: 'SG Eintracht Frankfurt', awayLeague: 1, result: '0:2', identifier: 2 },
+      { home: 'SV Waldhof Mannheim', homeLeague: 3, away: '1. FC Nürnberg', awayLeague: 2, result: '0:1', identifier: 2 },
+      { home: 'RB Leipzig', homeLeague: 1, away: 'Hamburger SV', awayLeague: 2, result: '4:0', identifier: 1 },
+      { home: 'SV Elversberg', homeLeague: 3, away: 'VfL Bochum', awayLeague: 1, result: '0:1', identifier: 2},
+      { home: 'Eintracht Braunschweig', homeLeague: 2, away: 'VfL Wolfsburg', awayLeague: 1, result: '1:2', identifier: 2 },
+      { home: 'TSG 1899 Hoffenheim', homeLeague: 1, away: 'FC Schalke 04', awayLeague: 1, result: '5:1', identifier: 1 },
+      { home: 'SV Darmstadt 98', homeLeague: 2, away: 'Borussia Mönchengladbach', awayLeague: 1, result: '2:1', identifier: 1 }
+    ],
     '1. Hauptrunde': [
-      { home: 'TSG Neustrelitz', homeLeague: '5NO', away: 'Karlsruher SC', awayLeague: 2, result: '0:8', homeWinner: false, awayWinner: true },
-      { home: '1. FC Kaan-Marienborn', homeLeague: '4W', away: '1. FC Nürnberg', awayLeague: 2, result: '0:2', homeWinner: false, awayWinner: true },
-      { home: 'Dynamo Dresden', homeLeague: 3, away: 'VfB Stuttgart', awayLeague: 1, result: '0:1', homeWinner: false, awayWinner: true },
-      { home: 'TSV 1860 München', homeLeague: '3', away: 'Borussia Dortmund', awayLeague: 1, result: '0:3', homeWinner: false, awayWinner: true },
-      { home: 'FC Viktoria 1889 Berlin', homeLeague: '4NO', away: 'VfL Bochum', awayLeague: 1, result: '0:3', homeWinner: false, awayWinner: true },
-      { home: 'SV Straelen', homeLeague: '4W', away: 'FC St. Pauli', awayLeague: 2, result: '3:4', homeWinner: false, awayWinner: true },
-      { home: 'SV Elversberg', homeLeague: 3, away: 'TSV Bayer 04 Leverkusen', awayLeague: 1, result: '4:3', homeWinner: true, awayWinner: false },
-      { home: 'SSV Jahn Regensburg', homeLeague: 2, away: '1. FC Köln', awayLeague: 1, result: '2:2 n.V. (2:2), 4:3 i.E.', homeWinner: true, awayWinner: false },
-      { home: 'FV Illertissen', homeLeague: '4BY', away: '1. FC Heidenheim', awayLeague: 2, result: '0:2', homeWinner: false, awayWinner: true },
-      { home: 'VfB Lübeck', homeLeague: '4N', away: 'FC Hansa Rostock', awayLeague: 2, result: '1:0', homeWinner: true, awayWinner: false },
-      { home: 'SpVgg Bayreuth', homeLeague: 3, away: 'Hamburger SV', awayLeague: 2, result: '1:3 n.V. (1:1)', homeWinner: false, awayWinner: true },
-      { home: 'FC Einheit Wernigerode', homeLeague: '5NO', away: 'SC Paderborn 07', awayLeague: 2, result: '0:10', homeWinner: false, awayWinner: true },
-      { home: 'Stuttgarter Kickers', homeLeague: '5BW', away: 'SpVgg Greuther Fürth', awayLeague: 2, result: '2:0', homeWinner: true, awayWinner: false },
-      { home: 'Kickers Offenbach', homeLeague: '4SW', away: 'Fortuna Düsseldorf', awayLeague: 2, result: '1:4', homeWinner: false, awayWinner: true },
-      { home: 'FC Carl Zeiss Jena', homeLeague: '4NO', away: 'VfL Wolfsburg', awayLeague: 2, result: '0:1', homeWinner: false, awayWinner: true },
-      { home: 'BSV Rehden', homeLeague: '4N', away: 'SV Sandhausen', awayLeague: 2, result: '0:4', homeWinner: false, awayWinner: true },
-      { home: 'Bremer SV', homeLeague: '4N', away: 'FC Schalke 04', awayLeague: 1, result: '0:5', homeWinner: false, awayWinner: true },
-      { home: '1. FC Kaiserslautern', homeLeague: 2, away: 'SC Freiburg', awayLeague: 1, result: '1:2 n.V. (1:1)', homeWinner: false, awayWinner: true },
-      { home: 'SV Oberachern', homeLeague: '5BW', away: 'Borussia Mönchengladbach', awayLeague: 1, result: '1:9', homeWinner: false, awayWinner: true },
-      { home: 'Blau-Weiß Lohne', homeLeague: '4N', away: 'FC Augsburg', awayLeague: 1, result: '0:4', homeWinner: false, awayWinner: true },
-      { home: 'TSV Schott Mainz', homeLeague: '5RS', away: 'Hannover 96', awayLeague: 2, result: '0:3', homeWinner: false, awayWinner: true },
-      { home: 'FV Engers', homeLeague: '5RS', away: 'Arminia Bielefeld', awayLeague: 2, result: '1:7', homeWinner: false, awayWinner: true },
-      { home: 'SV Rödinghausen', homeLeague: '4W', away: 'TSG 1899 Hoffenheim', awayLeague: 1, result: '0:2 n.V.', homeWinner: false, awayWinner: true },
-      { home: 'Eintracht Braunschweig', homeLeague: 2, away: 'Hertha BSC Berlin', awayLeague: 1, result: '4:4 n.V. (2:2), 6:5 i.E.', homeWinner: true, awayWinner: false },
-      { home: 'FC Erzgebirge Aue', homeLeague: 3, away: '1. FSV Mainz 05', awayLeague: 1, result: '0:3', homeWinner: false, awayWinner: true },
-      { home: 'SV Waldhof Mannheim', homeLeague: 3, away: 'Holstein Kiel', awayLeague: 2, result: '0:0 n.V., 5:3 i.E.', homeWinner: true, awayWinner: false },
-      { home: 'Chemnitzer FC', homeLeague: '4NO', away: '1. FC Union Berlin', awayLeague: 1, result: '1:2 n.V. (1:1)', homeWinner: false, awayWinner: true },
-      { home: 'FC Energie Cottbus', homeLeague: '4NO', away: 'SV Werder Bremen', awayLeague: 1, result: '1:2', homeWinner: false, awayWinner: true },
-      { home: 'FC Ingolstadt 04', homeLeague: 3, away: 'SV Darmstadt 98', awayLeague: 2, result: '0:3', homeWinner: false, awayWinner: true },
-      { home: '1. FC Magdeburg', homeLeague: 2, away: 'SG Eintracht Frankfurt', awayLeague: 1, result: '0:4', homeWinner: false, awayWinner: true },
-      { home: 'RB Leipzig', homeLeague: 1, away: 'FC Teutonia 05 Ottensen', awayLeague: '4N', result: '8:0', homeWinner: true, awayWinner: false },
-      { home: 'FC Viktoria Köln', homeLeague: 3, away: 'FC Bayern München', awayLeague: 1, result: '0:5', homeWinner: false, awayWinner: true }
+      { home: 'TSG Neustrelitz', homeLeague: '5NO', away: 'Karlsruher SC', awayLeague: 2, result: '0:8', identifier: 2 },
+      { home: '1. FC Kaan-Marienborn', homeLeague: '4W', away: '1. FC Nürnberg', awayLeague: 2, result: '0:2', identifier: 2 },
+      { home: 'Dynamo Dresden', homeLeague: 3, away: 'VfB Stuttgart', awayLeague: 1, result: '0:1', identifier: 2 },
+      { home: 'TSV 1860 München', homeLeague: '3', away: 'Borussia Dortmund', awayLeague: 1, result: '0:3', identifier: 2 },
+      { home: 'FC Viktoria 1889 Berlin', homeLeague: '4NO', away: 'VfL Bochum', awayLeague: 1, result: '0:3', identifier: 2 },
+      { home: 'SV Straelen', homeLeague: '4W', away: 'FC St. Pauli', awayLeague: 2, result: '3:4', identifier: 2 },
+      { home: 'SV Elversberg', homeLeague: 3, away: 'TSV Bayer 04 Leverkusen', awayLeague: 1, result: '4:3', identifier: 1 },
+      { home: 'SSV Jahn Regensburg', homeLeague: 2, away: '1. FC Köln', awayLeague: 1, result: '2:2 n.V. (2:2), 4:3 i.E.', identifier: 1 },
+      { home: 'FV Illertissen', homeLeague: '4BY', away: '1. FC Heidenheim', awayLeague: 2, result: '0:2', identifier: 2 },
+      { home: 'VfB Lübeck', homeLeague: '4N', away: 'FC Hansa Rostock', awayLeague: 2, result: '1:0', identifier: 1 },
+      { home: 'SpVgg Bayreuth', homeLeague: 3, away: 'Hamburger SV', awayLeague: 2, result: '1:3 n.V. (1:1)', identifier: 2 },
+      { home: 'FC Einheit Wernigerode', homeLeague: '5NO', away: 'SC Paderborn 07', awayLeague: 2, result: '0:10', identifier: 2 },
+      { home: 'Stuttgarter Kickers', homeLeague: '5BW', away: 'SpVgg Greuther Fürth', awayLeague: 2, result: '2:0', identifier: 1 },
+      { home: 'Kickers Offenbach', homeLeague: '4SW', away: 'Fortuna Düsseldorf', awayLeague: 2, result: '1:4', identifier: 2 },
+      { home: 'FC Carl Zeiss Jena', homeLeague: '4NO', away: 'VfL Wolfsburg', awayLeague: 2, result: '0:1', identifier: 2 },
+      { home: 'BSV Rehden', homeLeague: '4N', away: 'SV Sandhausen', awayLeague: 2, result: '0:4', identifier: 2 },
+      { home: 'Bremer SV', homeLeague: '4N', away: 'FC Schalke 04', awayLeague: 1, result: '0:5', identifier: 2 },
+      { home: '1. FC Kaiserslautern', homeLeague: 2, away: 'SC Freiburg', awayLeague: 1, result: '1:2 n.V. (1:1)', identifier: 2 },
+      { home: 'SV Oberachern', homeLeague: '5BW', away: 'Borussia Mönchengladbach', awayLeague: 1, result: '1:9', identifier: 2 },
+      { home: 'Blau-Weiß Lohne', homeLeague: '4N', away: 'FC Augsburg', awayLeague: 1, result: '0:4', identifier: 2 },
+      { home: 'TSV Schott Mainz', homeLeague: '5RS', away: 'Hannover 96', awayLeague: 2, result: '0:3', identifier: 2 },
+      { home: 'FV Engers', homeLeague: '5RS', away: 'Arminia Bielefeld', awayLeague: 2, result: '1:7', identifier: 2 },
+      { home: 'SV Rödinghausen', homeLeague: '4W', away: 'TSG 1899 Hoffenheim', awayLeague: 1, result: '0:2 n.V.', identifier: 2 },
+      { home: 'Eintracht Braunschweig', homeLeague: 2, away: 'Hertha BSC Berlin', awayLeague: 1, result: '4:4 n.V. (2:2), 6:5 i.E.', identifier: 1 },
+      { home: 'FC Erzgebirge Aue', homeLeague: 3, away: '1. FSV Mainz 05', awayLeague: 1, result: '0:3', identifier: 2 },
+      { home: 'SV Waldhof Mannheim', homeLeague: 3, away: 'Holstein Kiel', awayLeague: 2, result: '0:0 n.V., 5:3 i.E.', identifier: 1 },
+      { home: 'Chemnitzer FC', homeLeague: '4NO', away: '1. FC Union Berlin', awayLeague: 1, result: '1:2 n.V. (1:1)', identifier: 2 },
+      { home: 'FC Energie Cottbus', homeLeague: '4NO', away: 'SV Werder Bremen', awayLeague: 1, result: '1:2', identifier: 2 },
+      { home: 'FC Ingolstadt 04', homeLeague: 3, away: 'SV Darmstadt 98', awayLeague: 2, result: '0:3', identifier: 2 },
+      { home: '1. FC Magdeburg', homeLeague: 2, away: 'SG Eintracht Frankfurt', awayLeague: 1, result: '0:4', identifier: 2 },
+      { home: 'RB Leipzig', homeLeague: 1, away: 'FC Teutonia 05 Ottensen', awayLeague: '4N', result: '8:0', identifier: 1 },
+      { home: 'FC Viktoria Köln', homeLeague: 3, away: 'FC Bayern München', awayLeague: 1, result: '0:5', identifier: 2 }
     ]
   }
   return (
     <main className={styles.main}>
       <h1 className={styles.h1}>DFB-Pokal 2022/23</h1>
-      <h2 className={styles.h2}>1. Hauptrunde</h2>
-      {leagueIdentifiers.map(hleague => {
-        return leagueIdentifiers.map(aleague => {
-          const hamatches = data['1. Hauptrunde'].filter(match => `${match.homeLeague}` === `${hleague}` && `${match.awayLeague}` === `${aleague}`)
-          const hlname = leagues[hleague]
-          const alname = leagues[aleague]
+      <h2 className={styles.h2}>2. Hauptrunde</h2>
+      {leagueIdentifiers.map((hleague, idx: number) => {
+        return leagueIdentifiers.slice(idx).map(aleague => {
+          const hamatches = data['2. Hauptrunde'].filter(match => (`${match.homeLeague}` === `${hleague}` && `${match.awayLeague}` === `${aleague}`) || (`${match.homeLeague}` === `${aleague}` && `${match.awayLeague}` === `${hleague}`))
+          const hindex = leagueIdentifiers.indexOf(hleague)
+          const aindex = leagueIdentifiers.indexOf(aleague)
+          const mindex = Math.min(hindex, aindex)
+          const pindex = Math.max(hindex, aindex)
+          const mkey = hindex === mindex ? hleague : aleague
+          const pkey = hindex === pindex ? hleague : aleague
+          const loname = hindex <= aindex ? leagues[hleague] : leagues[aleague]
+          const hiname = hindex > aindex ? leagues[hleague] : leagues[aleague]
           if (hamatches.length > 0) {
             return (
-              <div key={`${hleague}_${aleague}`}>
-                <h3 className={styles.h3}>{hlname} – {alname}</h3>
+              <div key={`${mkey}_${pkey}`}>
+                <h3 className={styles.h3}>{loname} – {hiname}</h3>
                 {hamatches.map((match, index) => {
                   return (
-                    <div className={styles.flex} key={`${hleague}_${aleague}_${index}`}>
-                      <span className={match.homeWinner ? styles.winner : undefined}>{match.home}</span>
-                      <span className={match.awayWinner ? styles.winner : undefined}>{match.away}</span>
+                    <div className={styles.flex} key={`${mkey}_${pkey}_${index}`}>
+                      <span className={match.identifier === 1 ? styles.winner : undefined}>{match.home}</span>
+                      <span className={match.identifier === 2 ? styles.winner : undefined}>{match.away}</span>
+                      <span>{match.result ? match.result : "–:–"}</span>
+                    </div>
+                  )})}
+              </div>
+            )
+          }
+        })
+      })}
+      <h2 className={styles.h2}>1. Hauptrunde</h2>
+      {leagueIdentifiers.map((hleague, idx: number) => {
+        return leagueIdentifiers.slice(idx).map(aleague => {
+          const hamatches = data['1. Hauptrunde'].filter(match => (`${match.homeLeague}` === `${hleague}` && `${match.awayLeague}` === `${aleague}`) || (`${match.homeLeague}` === `${aleague}` && `${match.awayLeague}` === `${hleague}`))
+          const hindex = leagueIdentifiers.indexOf(hleague)
+          const aindex = leagueIdentifiers.indexOf(aleague)
+          const mindex = Math.min(hindex, aindex)
+          const pindex = Math.max(hindex, aindex)
+          const mkey = hindex === mindex ? hleague : aleague
+          const pkey = hindex === pindex ? hleague : aleague
+          const loname = hindex <= aindex ? leagues[hleague] : leagues[aleague]
+          const hiname = hindex > aindex ? leagues[hleague] : leagues[aleague]
+          if (hamatches.length > 0) {
+            return (
+              <div key={`${mkey}_${pkey}`}>
+                <h3 className={styles.h3}>{loname} – {hiname}</h3>
+                {hamatches.map((match, index) => {
+                  return (
+                    <div className={styles.flex} key={`${mkey}_${pkey}_${index}`}>
+                      <span className={match.identifier === 1 ? styles.winner : undefined}>{match.home}</span>
+                      <span className={match.identifier === 2 ? styles.winner : undefined}>{match.away}</span>
                       <span>{match.result ? match.result : "–:–"}</span>
                     </div>
                   )})}
