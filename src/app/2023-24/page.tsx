de610c4847d43ffbe69a06942dac006e187d0fd1
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-08-12 17:47:03 +0200

    DFB-Pokal, season 2023/24, Round 1: results of 2023-08-12T13:30Z matches

--- src/app/2023-24/page.tsx
+++ src/app/2023-24/page.tsx
@@ -11,6 +11,7 @@ type Match = {
   result?: string
   identifier?: number
   date?: string
+  live?: boolean
 }
 
 type Data = { [key: string]: Match[] }
@@ -21,6 +22,7 @@ type MatchProps = {
   result?: string
   identifier?: number
   date?: string
+  live?: boolean
 }
 
 type RoundProps = {
@@ -28,14 +30,14 @@ type RoundProps = {
   title: string
 }
 
-function Match({home, away, result, identifier, date}: MatchProps) {
+function Match({home, away, result, identifier, date, live}: MatchProps) {
   const tz = Intl.DateTimeFormat().resolvedOptions().timeZone
   const formattedDate = date ? new Date(date).toLocaleString("de", { year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit", timeZone: tz }) : null
   return (
     <div className={styles.flex}>
       <span className={identifier === 1 ? styles.winner : undefined}>{home}</span>
       <span className={identifier === 2 ? styles.winner : undefined}>{away}</span>
-      <span>{result ? result : formattedDate ? formattedDate : "–:–"}</span>
+      <span className={live ? styles.winner : undefined}>{result ? result : formattedDate ? formattedDate : "–:–"}</span>
       {result && <span>{formattedDate}</span>}
     </div>
   )
@@ -95,11 +97,11 @@ function Round({matches, title}: RoundProps) {
                         {hamatches.map((match, index) => {
                           if (match.date) {
                             return (
-                              <Match home={match.home} away={match.away} result={match.result} identifier={match.identifier} date={match.date} key={`${mkey}_${pkey}_${index}`} />
+                              <Match home={match.home} away={match.away} result={match.result} identifier={match.identifier} date={match.date} key={`${mkey}_${pkey}_${index}`} live={match.live} />
                             )
                           }
                           return (
-                              <Match home={match.home} away={match.away} result={match.result} identifier={match.identifier} key={`${mkey}_${pkey}_${index}`} />
+                              <Match home={match.home} away={match.away} result={match.result} identifier={match.identifier} key={`${mkey}_${pkey}_${index}`} live={match.live} />
                             )
                         })}
                       </div>
@@ -140,7 +142,7 @@ export default function DFBPokal_2023_24() {
       { home: 'Hallescher FC', homeLeague: 3, away: 'SpVgg Greuther Fürth', awayLeague: 2, date: "2023-08-12T16:00:00Z" },
       { home: 'SV Sandhausen', homeLeague: 3, away: 'Hannover 96', awayLeague: 2, date: "2023-08-11T16:00:00Z", result: "3:3 n.V. (3:3), 4:2 i.E.", identifier: 1 },
       { home: 'VfL Osnabrück', homeLeague: 2, away: '1. FC Köln', awayLeague: 1, date: "2023-08-14T16:00:00Z" },
-      { home: 'SV Atlas Delmenhorst', homeLeague: '5NI', away: 'FC St. Pauli', awayLeague: 2, date: "2023-08-12T13:30:00Z" },
+      { home: 'SV Atlas Delmenhorst', homeLeague: '5NI', away: 'FC St. Pauli', awayLeague: 2, date: "2023-08-12T13:30:00Z", result: "0:5", identifier: 2 },
       { home: 'FV Illertissen', homeLeague: '4BY', away: 'Fortuna Düsseldorf', awayLeague: 2, date: "2023-08-13T13:30:00Z" },
       { home: 'TuS Makkabi Berlin', homeLeague: '5NO', away: 'VfL Wolfsburg', awayLeague: 1, date: "2023-08-13T13:30:00Z" },
       { home: 'FC Rot-Weiß Koblenz', homeLeague: '5RS', away: '1. FC Kaiserslautern', awayLeague: 2, date: "2023-08-13T13:30:00Z" },
@@ -149,21 +151,21 @@ export default function DFBPokal_2023_24() {
       { home: 'Preußen Münster', homeLeague: 3, away: 'FC Bayern München', awayLeague: 1, date: "2023-09-26T18:45:00Z" },
       { home: 'SSV Jahn Regensburg', homeLeague: 3, away: '1. FC Magdeburg', awayLeague: 2, date: "2023-08-14T16:00:00Z" },
       { home: 'FC Energie Cottbus', homeLeague: '4NO', away: 'SC Paderborn 07', awayLeague: 2, date: "2023-08-13T16:00:00Z" },
-      { home: 'FC Oberneuland', homeLeague: '5HB', away: '1. FC Nürnberg', awayLeague: 2, date: "2023-08-12T13:30:00Z" },
+      { home: 'FC Oberneuland', homeLeague: '5HB', away: '1. FC Nürnberg', awayLeague: 2, date: "2023-08-12T13:30:00Z", result: "1:9", identifier: 2 },
       { home: 'VfB Lübeck', homeLeague: 3, away: 'TSG 1899 Hoffenheim', awayLeague: 1, date: "2023-08-14T16:00:00Z" },
       { home: 'TSG Balingen', homeLeague: '4SW', away: 'VfB Stuttgart', awayLeague: 1, date: "2023-08-12T11:00:00Z", result: "0:4", identifier: 2 },
-      { home: 'TSV Schott Mainz', homeLeague: '4SW', away: 'Borussia Dortmund', awayLeague: 1, date: "2023-08-12T13:30:00Z" },
+      { home: 'TSV Schott Mainz', homeLeague: '4SW', away: 'Borussia Dortmund', awayLeague: 1, date: "2023-08-12T13:30:00Z", result: "1:6", identifier: 2 },
       { home: 'Rot-Weiss Essen', homeLeague: 3, away: 'Hamburger SV', awayLeague: 2, date: "2023-08-13T11:00:00Z" },
       { home: 'SV Elversberg', homeLeague: 2, away: '1. FSV Mainz 05', awayLeague: 1, date: "2023-08-12T16:00:00Z" },
       { home: '1. FC Lokomotive Leipzig', homeLeague: '4NO', away: 'SG Eintracht Frankfurt', awayLeague: 1, date: "2023-08-13T13:30:00Z" },
       { home: 'FC Astoria Walldorf', homeLeague: '4SW', away: '1. FC Union Berlin', awayLeague: 1, date: "2023-08-13T16:00:00Z" },
-      { home: 'FC Viktoria Köln', homeLeague: 3, away: 'SV Werder Bremen', awayLeague: 1, date: "2023-08-12T13:30:00Z" },
+      { home: 'FC Viktoria Köln', homeLeague: 3, away: 'SV Werder Bremen', awayLeague: 1, date: "2023-08-12T13:30:00Z", result: "3:2", identifier: 1 },
       { home: 'FSV Frankfurt', homeLeague: '4SW', away: 'FC Hansa Rostock', awayLeague: 2, date: "2023-08-13T16:00:00Z" },
       { home: 'SV Wehen Wiesbaden', homeLeague: 2, away: 'RB Leipzig', awayLeague: 1, date: "2023-09-27T18:45:00Z" },
-      { home: 'FC Teutonia 05 Ottensen', homeLeague: '4N', away: 'TSV Bayer 04 Leverkusen', awayLeague: 1, date: "2023-08-12T13:30:00Z" },
+      { home: 'FC Teutonia 05 Ottensen', homeLeague: '4N', away: 'TSV Bayer 04 Leverkusen', awayLeague: 1, date: "2023-08-12T13:30:00Z", result: "0:8", identifier: 2 },
       { home: 'FC Carl Zeiss Jena', homeLeague: '4NO', away: 'Hertha BSC Berlin', awayLeague: 2, date: "2023-08-12T11:00:00Z", result: "0:5", identifier: 2 },
       { home: '1. FC Saarbrücken', homeLeague: 3, away: 'Karlsruher SC', awayLeague: 2, date: "2023-08-11T16:00:00Z", result: "2:1", identifier: 1 },
-      { home: 'FC Gütersloh', homeLeague: '4W', away: 'Holstein Kiel', awayLeague: 2, date: "2023-08-12T13:30:00Z" },
+      { home: 'FC Gütersloh', homeLeague: '4W', away: 'Holstein Kiel', awayLeague: 2, date: "2023-08-12T13:30:00Z", result: "0:2", identifier: 2 },
       { home: 'SV Oberachern', homeLeague: '5BW', away: 'SC Freiburg', awayLeague: 1, date: "2023-08-13T13:30:00Z" },
       { home: 'TuS Bersenbrück', homeLeague: '5NI', away: 'Borussia Mönchengladbach', awayLeague: 1, date: "2023-08-11T16:00:00Z", result: "0:7", identifier: 2 },
       { home: 'Arminia Bielefeld', homeLeague: 3, away: 'VfL Bochum', awayLeague: 1, date: "2023-08-12T16:00:00Z" },
