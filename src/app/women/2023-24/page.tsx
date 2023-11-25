69d9bbf14a71382b54b1e71fb914834c6b56fcdc
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-11-25 21:40:52 +0100

    DFB-Pokal Women, season 2023/24, round of 16: results of matches on 2023-11-25T13:00Z

--- src/app/women/2023-24/page.tsx
+++ src/app/women/2023-24/page.tsx
@@ -7,13 +7,13 @@ import styles from '@/styles/index.module.css'
 export default function DFBPokal_2023_24() {
   const data: Data = {
     'Achtelfinale': [
-      { home: 'Hamburger SV', homeLeague: '2', away: 'Bayer 04 Leverkusen', awayLeague: '1', date: '2023-11-25T13:00Z' },
+      { home: 'Hamburger SV', homeLeague: '2', away: 'Bayer 04 Leverkusen', awayLeague: '1', date: '2023-11-25T13:00Z', result: '0:4', identifier: 2 },
       { home: 'TSG 1899 Hoffenheim', homeLeague: '1', away: 'RB Leipzig', awayLeague: '1', date: '2023-11-26T12:00Z' },
       { home: '1. FSV Mainz 05', homeLeague: '2SW', away: 'MSV Duisburg', awayLeague: '1', date: '2023-11-26T14:00Z' },
       { home: 'SG Eintracht Frankfurt', homeLeague: '1', away: 'SC Freiburg', awayLeague: '1', date: '2024-01-22T17:30Z' },
       { home: 'VfL Wolfsburg', homeLeague: '1', away: 'SV Werder Bremen', awayLeague: '1', date: '2023-11-24T17:00Z', result: '5:0', identifier: 1 },
-      { home: 'FC Viktoria 1889 Berlin', homeLeague: '3NO', away: 'FC Carl Zeiss Jena', awayLeague: '2', date: '2023-11-25T13:00Z' },
-      { home: 'SGS Essen', homeLeague: '1', away: '1. FC Köln', awayLeague: '1', date: '2023-11-25T13:00Z' },
+      { home: 'FC Viktoria 1889 Berlin', homeLeague: '3NO', away: 'FC Carl Zeiss Jena', awayLeague: '2', date: '2023-11-25T13:00Z', result: '1:3', identifier: 2 },
+      { home: 'SGS Essen', homeLeague: '1', away: '1. FC Köln', awayLeague: '1', date: '2023-11-25T13:00Z', result: '4:3', identifier: 1 },
       { home: 'Kickers Offenbach', homeLeague: '3S', away: 'FC Bayern München', awayLeague: '1', date: '2024-01-21T13:00Z' }
     ],
     '2. Hauptrunde': [
