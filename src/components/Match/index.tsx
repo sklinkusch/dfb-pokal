8458c5075528cf8199a73870a21d50749387a17f
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2024-04-25 14:05:24 +0200

    show weekday in match dates

--- src/components/Match/index.tsx
+++ src/components/Match/index.tsx
@@ -2,7 +2,7 @@ import styles from "./index.module.css"
 
 function Match({home, away, result, identifier, date, live}: MatchProps) {
   const tz = Intl.DateTimeFormat().resolvedOptions().timeZone
-  const formattedDate = date ? new Date(date).toLocaleString("de", { year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit", timeZone: tz }) : null
+  const formattedDate = date ? new Date(date).toLocaleString("de", { year: "numeric", month: "2-digit", day: "2-digit", weekday: "short", hour: "2-digit", minute: "2-digit", timeZone: tz }) : null
   return (
     <div className={styles.flex}>
       <span className={identifier === 1 ? styles.winner : undefined}>{home}</span>
