fe8b975235078de3d9b70721f9aac7fee1fba3c2
Stefan Klinkusch <42465490+sklinkusch@users.noreply.github.com>
2026-09-02 11:23:45 +0200

    DFB-Pokal Men, season 2026/27, 1st round: add result of 2026-09-01T18:45Z

--- src/app/men/2026-27/page.tsx
+++ src/app/men/2026-27/page.tsx
@@ -148,7 +148,7 @@ export default function DFBPokal_2024_25() {
       getMatch('AGL', 'WOB', '2026-08-24T16:00Z', '2:2, 3:3 n.V., 5:6 i.E.', 2),
       getMatch('SCV', 'HSV', '2026-08-24T16:00Z', '0:3', 2),
       getMatch('HFC', 'S04', '2026-08-24T18:45Z', '2:2, 2:5 n.V.', 2),
-      getMatch('HEB', 'BVB', '2026-09-01T18:45Z'),
+      getMatch('HEB', 'BVB', '2026-09-01T18:45Z', '0:5', 2),
       getMatch('OSN', 'FCB', '2026-09-02T18:45Z'),
     ]
   };
